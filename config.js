0c0e979c619e3cd4850cb76e76727f88ef04936f
Wasley <104096743+Wasleycik@users.noreply.github.com>
2023-07-21 20:49:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 prefix:".", //Botun Prefix'i 
-token: "ODgwODA4NzAzODQ1MDg5Mjgw.GFrfhA.kQqDgoh57xB1U7I3lLll2yECswhs8vCA1KgIxQ", //Bot Tokeni
+token: "", //Bot Tokeni
 guildID:"901817874220453899", //Kullanıcağın Sunucunun ID'si
 botOwners:["928259219038302258"], //Bot Sahibinin ID'si
 botDurum:"Hesperos ❤️ Wasley", //Botun Durumu
