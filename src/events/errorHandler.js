fd8c13b39bd2cc8c64864b037a6edd32ab682170
Wasley <104096743+Wasleycik@users.noreply.github.com>
2023-09-25 22:03:09 +0300

    Delete src directory

--- src/events/errorHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function(e){
-console.log(`⚠️ App_Error; `+e)}
-
-module.exports.conf = {
-name: "error"
-}
--- src/events/rateLimit.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function(RateLimitData) {
-console.log("⚠️ Rate_Limit_Warn; ", RateLimitData)}
-module.exports.conf = {
-name: "rateLimit"
-}
